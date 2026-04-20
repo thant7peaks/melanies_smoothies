2aefa7cda80c3ca89e90b744edf9283ff63a994f
thant7peaks <extthant.p@7peakssoftware.com>
2026-04-20 16:45:18 +0700

    Update pending_orders.py

--- pending_orders.py
+++ pending_orders.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 # Write directly to the app
 
@@ -13,8 +12,8 @@ st.write(
 )
 
 
-
-session = get_active_session()
+cnx=st.connection("snowflake")
+session =cnx.session()
 
 
 
