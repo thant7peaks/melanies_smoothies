a8c4543e4199cc2b58ff1534566db5adda38855e
thant7peaks <extthant.p@7peakssoftware.com>
2026-04-21 11:01:57 +0700

    Update custom_smoothie_order_form.py

--- custom_smoothie_order_form.py
+++ custom_smoothie_order_form.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 # Write directly to the app
 st.title(f" Customize Your Smoothie!:cup_with_straw: ")
 st.write(
@@ -22,6 +23,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         # Correctly add the fruit name AND a space to the string
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen+'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)  
+        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
     # Show the string on the screen like in the picture
     # st.write(ingredients_string)
@@ -34,7 +39,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-    import requests  
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    # st.text(smoothiefroot_response)
-    st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+     
+    
